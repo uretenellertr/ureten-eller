73a4612de4760eb064bb4c42e009264eb7443f65
uretenellertr <uretenellertr@gmail.com>
2025-10-07 22:10:44 +0000

    publish: portal home + customer/seller

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+};
 module.exports = nextConfig;
