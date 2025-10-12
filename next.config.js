02032f0ef0c4f76b69031ffe21bcae6c32a8922c
uretenellertr <uretenellertr@gmail.com>
2025-10-12 14:17:46 +0000

    chore: wip before deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',        // build sırasında statik export yap
-  trailingSlash: true,     // GitHub Pages 404 olmasın
-  images: { unoptimized: true }
+module.exports = {
+  output: 'export',
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
-module.exports = nextConfig;
