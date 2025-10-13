54df8ca4151e91c66506f11dafe8499e85bfd08b
uretenellertr <uretenellertr@gmail.com>
2025-10-13 16:50:28 +0000

    deploy: static export to docs (next14 + tailwind v4)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' };
+const nextConfig = { output: 'export', trailingSlash: true, images: { unoptimized: true } };
 module.exports = nextConfig;
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,2 @@
 /* Tailwind v4+ PostCSS config */
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-};
+module.exports = { plugins: { '@tailwindcss/postcss': {}, autoprefixer: {} } };
