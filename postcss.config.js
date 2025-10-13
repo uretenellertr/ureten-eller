5cea26513c027b9a34c473cd0ac10faf2ab985bc
uretenellertr <uretenellertr@gmail.com>
2025-10-13 16:37:24 +0000

    publish: update site

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,7 @@
-module.exports={plugins:{tailwindcss:{},autoprefixer:{}}};
+/** Tailwind v4+ PostCSS config */
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
