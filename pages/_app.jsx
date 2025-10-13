d0049be00f91a5526c1c667055be1f603d54112e
uretenellertr <uretenellertr@gmail.com>
2025-10-13 13:56:01 +0000

    fix: Tailwind+PostCSS ensured, export w/ trailingSlash

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,2 +1,1 @@
-import "../styles/globals.css";
-export default function App({ Component, pageProps }){ return <Component {...pageProps} />; }
+import "../styles/globals.css"; export default function App({Component,pageProps}){ return <Component {...pageProps}/> };
---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+module.exports={content:["./pages/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],theme:{extend:{}},plugins:[]};
